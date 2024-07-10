72a9ba15a007b97bd061bb2f8e4d5959f0c3c34c
Gustav Hartvigsson <gustav.hartvigsson@gmail.com>
2024-07-10 22:46:40 +0200

    * added bash function libary.

---
+++ scripts/useful.inc.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+####
+# FILE NAME useful.inc.sh
+# 
+# PURPOSE
+#   This file provides usefule bash libary functions.
+####
+# Add the following to your script to be able to include it:
+#     __SCRIPT_ROOT=$(dirname $(readlink -f $0))
+#     source $__SCRIPT_ROOT/useful.inc.sh
+####
+
+####
+# FUNCTION __silent
+#
+# PURPOSE
+#   Silent a run command.
+#
+# ARGUMENTS
+#   A command and it's arguments.
+#
+# RETURNS
+#   Exit code of command.
+#
+####
+function __silent () {
+  $@ >> /dev/null 2>&1
+  return $?
+}
+
+####
+# FUNCTION __find_tool
+#
+# PURPOSE
+#   checks if a tool (progam) exists.
+#
+# NOTE
+#   You need to specify "__SANITY=true" among your globals.
+####
+function __find_tool () {
+  __silent which $1
+
+  if [ $? -gt 0 ]; then
+    echo "    Can't find tool \"${1}\"."
+    __SANITY=false
+  fi
+}
